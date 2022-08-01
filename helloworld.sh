4862b3a065cfe39b76c87d564788fa380ba4ca0d
neekdasneak <armandnicco22n@icloud.com>
2022-08-01 05:32:48 +0000

    changed date

--- helloworld.sh
+++ helloworld.sh
@@ -2,7 +2,7 @@
 
 # Script: Ops 201 Class 02 Ops Challenge Solution
 # Author: Nicco Evans
-# Date of latest revision: 7/37/22
+# Date of latest revision: 7/31/22
 # Purpose: Print a string to the terminal
 
 # Main
